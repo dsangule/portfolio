6c07f4994595a81d8eea7313750751eee0fd2550
dsangule <114325989+dsangule@users.noreply.github.com>
2023-01-30 11:15:25 +0530

    links and CSS on NavBar, changed icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
         <ul>
-          <li>Home</li>
-          <li>About Me</li>
-          <li>Contact</li>
+            <Link href={'/'}>
+              <li>Home</li>
+            </Link>
+            <Link href={'/about'}>
+              <li>About</li>
+            </Link>
+            <Link href={'/contact'}>
+              <li>Contact</li>
+            </Link>
         </ul>
     </nav>
   )
