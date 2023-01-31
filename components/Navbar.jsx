0f606bbeb53bab4fa3db5ef31f24cc5a313671af
dsangule <114325989+dsangule@users.noreply.github.com>
2023-01-31 11:12:25 +0530

    Added icon, more mobile responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <ul>
-            <Link href={'/'}>
-              <li>Home</li>
-            </Link>
-            <Link href={'/about'}>
-              <li>About</li>
-            </Link>
-            <Link href={'/contact'}>
-              <li>Contact</li>
-            </Link>
-        </ul>
+      <div className={styles.navIconFlex}>
+        <div className={styles.navIcon}>
+          <Image src='/../public/TempIcon.png' width={40} height={40}></Image>
+        </div>
+      </div>
+      <ul>
+        <Link href={'/'}>
+          <li>Home</li>
+        </Link>
+        <Link href={'/about'}>
+          <li>About</li>
+        </Link>
+        <Link href={'/contact'}>
+          <li>Contact</li>
+        </Link>
+      </ul>
     </nav>
   )
 }
