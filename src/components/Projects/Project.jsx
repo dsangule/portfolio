42bbea9a1edf091ab7baef4ca1c33acf08c0baeb
dsangule <114325989+dsangule@users.noreply.github.com>
2024-07-14 23:19:39 +0530

    updated github icon on project

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,24 +1,27 @@
 import Image from "next/image"
+import Link from "next/link"
 import StackIcon from "tech-stack-icons"
 
 function Project() {
   return (
     <div className="bg-black flex flex-col md:flex-row text-white rounded-md">
-        <Image src={"/void-hack-genesis.png"} height={500} width={500} className="w-[100%] md:w-[40%] mr-2 rounded-md" />
-        <div className="p-2">
-            <p className="text-xl font-oswald">
-                First Responders Alliance
+      <Image src={"/void-hack-genesis.png"} height={500} width={500} className="w-[100%] md:w-[40%] mr-2 rounded-md" />
+      <div className="p-2">
+        <p className="text-xl font-oswald">
+          First Responders Alliance
+        </p>
+        <p className="font-akshar font-extralight">
+            A project made for hack genesis, an online hackathon conducted on Devfolio.
+        </p>
+        <div>
+          <Link href={"https://github.com/dsangule/void-hack-genesis"} className="bg-white text-black text-sm p-0 flex w-fit pr-1 rounded-full gap-1">
+            <StackIcon name="github" className="w-6" />
+            <p className="content-center h-6">
+              Github Repository
             </p>
-            <p className="font-akshar font-extralight">
-                A project made for hack genesis, an online hackathon conducted on Devfolio.
-            </p>
-            <div className="bg-white text-black text-sm p-0 flex w-fit pr-1 rounded-full gap-1">
-              <StackIcon name="github" className="w-6" />
-              <p className="text-center h-fit">
-                https://github.com/dsangule/void-hack-genesis
-              </p>
-            </div>
+          </Link>
         </div>
+      </div>
     </div>
   )
 }
