84c8e9f36d7188eaddd20f825014fe90944f1744
dsangule <114325989+dsangule@users.noreply.github.com>
2024-07-15 23:46:31 +0530

    updated Projects section

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -6,15 +6,15 @@ function Project() {
   return (
     <div className="bg-black flex flex-col md:flex-row text-white rounded-md">
       <Image src={"/void-hack-genesis.png"} height={500} width={500} className="w-[100%] md:w-[40%] mr-2 rounded-md" />
-      <div className="p-2">
-        <p className="text-xl font-oswald">
+      <div className="p-4 flex flex-col w-full">
+        <p className="text-xl font-oswald mb-2">
           First Responders Alliance
         </p>
-        <p className="font-akshar font-extralight">
+        <p className="font-akshar font-extralight flex-grow">
             A project made for hack genesis, an online hackathon conducted on Devfolio.
         </p>
         <div>
-          <Link href={"https://github.com/dsangule/void-hack-genesis"} className="bg-white text-black text-sm p-0 flex w-fit pr-1 rounded-full gap-1">
+          <Link href={"https://github.com/dsangule/void-hack-genesis"} target="_blank" className="bg-white text-black text-xs p-0 flex w-fit pr-1 rounded-full gap-1">
             <StackIcon name="github" className="w-6" />
             <p className="content-center h-6">
               Github Repository
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -5,6 +5,10 @@ function Projects() {
     <div className="bg-white min-h-[50vh] px-[5vw] py-[5vw]">
       <p className="text-5xl font-akshar text-center">Projects</p><br />
       <Project />
+      <br/>
+      <Project />
+      <br/>
+      <Project />
     </div>
   )
 }
