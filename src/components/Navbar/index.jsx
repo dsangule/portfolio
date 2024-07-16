36bee362b10100db11d085ec27a038a061a85912
dsangule <114325989+dsangule@users.noreply.github.com>
2024-07-16 23:28:21 +0530

    updated navbar linking

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,15 @@
+"use client";
+
 import Link from "next/link"
 
 function Navbar() {
+
     return (
         <nav className="text-white text-sm sm:text-lg lg:text-xl font-oswald p-5 flex justify-end">
-            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="/">Skills</Link>
-            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="/">Education</Link>
-            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="/">Projects</Link>
-            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="/">Contact</Link>
+            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="#skills">Skills</Link>
+            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="#about">About</Link>
+            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="#projects">Projects</Link>
+            <Link className="px-1 sm:px-2 lg:px-4 hover:text-[#C8B8DB]" href="#contact">Contact</Link>
         </nav>
     )
 }
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,7 +2,7 @@ import Project from "./Project"
 
 function Projects() {
   return (
-    <div className="bg-white min-h-[50vh] px-[5vw] py-[5vw]">
+    <div className="bg-white min-h-[50vh] px-[5vw] py-[5vw]" id="projects">
       <p className="text-5xl font-akshar text-center">Projects</p><br />
       <Project />
       <br/>
