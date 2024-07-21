472ba6270299459629e62f4ce109f4b4985209a5
dsangule <114325989+dsangule@users.noreply.github.com>
2024-07-21 21:18:11 +0530

    updated Project to be reusable

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,19 +2,19 @@ import Image from "next/image"
 import Link from "next/link"
 import StackIcon from "tech-stack-icons"
 
-function Project() {
+function Project({item}) {
   return (
-    <div className="bg-black flex flex-col md:flex-row text-white rounded-md">
-      <Image src={"/void-hack-genesis.png"} height={500} width={500} className="w-[100%] md:w-[40%] mr-2 rounded-md" />
+    <div className="bg-black flex flex-col md:flex-row text-white rounded-md md:h-60">
+      <Image src={item.imgsrc} height={500} width={500} className="w-[100%] md:w-[40%] mr-2 rounded-md" />
       <div className="p-4 flex flex-col w-full">
         <p className="text-xl font-oswald mb-2">
-          First Responders Alliance
+          {item.title}
         </p>
         <p className="font-akshar font-extralight flex-grow">
-            A project made for hack genesis, an online hackathon conducted on Devfolio.
+          {item.desc}
         </p>
         <div>
-          <Link href={"https://github.com/dsangule/void-hack-genesis"} target="_blank" className="bg-white text-black text-xs p-0 flex w-fit pr-1 rounded-full gap-1">
+          <Link href={item.link} target="_blank" className="bg-white text-black text-xs p-0 flex w-fit pr-1 rounded-full gap-1">
             <StackIcon name="github" className="w-6" />
             <p className="content-center h-6">
               Github Repository
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,14 +1,33 @@
 import Project from "./Project"
 
 function Projects() {
+
+  const projects = [
+    {
+      key: 1,
+      title: "First Responders Alliance",
+      desc: "A project made for hack genesis, an online hackathon conducted on Devfolio.",
+      imgsrc: "/void-hack-genesis.png",
+      link: "https://github.com/dsangule/void-hack-genesis",
+    },
+    {
+      key: 2,
+      title: "RoboWars",
+      desc: "A First Person Shooter multiplayer game made using Unity engine for Student Mentorship Programme by Loop Club of School of Engineering, Jawaharlal Nehru University, Delhi.",
+      imgsrc: "/RoboWarsCover.png",
+      link: "https://github.com/dsangule/RoboWars",
+    },
+  ]
+
   return (
     <div className="bg-white min-h-[50vh] px-[5vw] py-[5vw]" id="projects">
       <p className="text-5xl font-akshar text-center">Projects</p><br />
-      <Project />
-      <br/>
-      <Project />
-      <br/>
-      <Project />
+      {projects.map((project)=>{
+        return <>
+          <Project key={project.key} item={project} />
+          <br />
+        </>
+      })}
     </div>
   )
 }
