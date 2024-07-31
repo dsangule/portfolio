a12df2090a06fbba64268d3d784c59e33cef627f
dsangule <114325989+dsangule@users.noreply.github.com>
2024-07-31 23:20:48 +0530

    updated hero, highlight name

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <Navbar />
       <div className="mx-auto w-[70vw] h-full font-anek font-light tracking-tighter text-center text-xl sm:text-3xl">
         <div className="absolute w-[70vw] top-1/2 -translate-y-1/2">
-          Welcome to my digital portfolio! My name is Devansh Sangule and I am a second-year college student passionate about software development. Come and explore my journey in software development.
+          Welcome to my digital portfolio! My name is <span className="font-semibold text-indigo-950">Devansh Sangule</span> and I am a third-year college student passionate about software development. Come and explore my journey in software development.
         </div>
       </div>
     </div>
