b8dddc30d2d4cf5786b0e779830ec6d0f41f0619
dsangule <dsangule@gmail.com>
2025-02-04 18:14:56 +0530

    updated nextjs, removed tech-stack-icons

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,11 +1,10 @@
 import Image from "next/image"
 import Link from "next/link"
-import StackIcon from "tech-stack-icons"
 
 function Project({item}) {
   return (
     <div className="bg-black flex flex-col md:flex-row text-white rounded-md md:h-60">
-      <Image src={item.imgsrc} height={500} width={300} className="w-[100%] md:max-w-96 md:min-w-96 mr-2 rounded-md" />
+      <Image src={item.imgsrc} alt="Project image" height={500} width={500} className="w-[100%] h-[100%] md:max-w-96 md:min-w-96 mr-2 rounded-md" />
       <div className="p-4 pr-6 flex flex-col w-full">
         <p className="text-xl font-oswald mb-1 md:mb-2">
           {item.title}
@@ -15,7 +14,7 @@ function Project({item}) {
         </p>
         <div className="mt-4">
           <Link href={item.link} target="_blank" className="bg-white text-black text-xs p-0 flex w-fit pr-1 rounded-full gap-1">
-            <StackIcon name="github" className="w-6" />
+          <Image src={"/GitHub.svg"} alt="Github icon" height={300} width={300} className="w-6" />
             <p className="content-center h-6">
               Github Repository
             </p>
